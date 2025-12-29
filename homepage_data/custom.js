17587b1d0f9b78fe2e2353e2eb0c5c7941a59c73
Lucas C. Noman <lucas.noman@outlook.com>
2025-12-29 15:18:08 -0300

    refactor: adds Tianji monitor

---
+++ homepage_data/custom.js
@@ -0,0 +1,8 @@
+// Adds Tianji monitoring
+const script = document.createElement('script')
+script.async = true
+script.defer = true
+script.src = 'http://192.168.2.100:12345/tracker.js'
+script.setAttribute('data-website-id', 'cmehj8g12001vwcsc21wm7c09')
+
+document.head.appendChild(script)
